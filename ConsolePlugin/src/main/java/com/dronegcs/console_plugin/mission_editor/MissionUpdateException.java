7b0a120e87a3baf6a8983e39cc2e18d6ec6507a8
taljmars <taljmars@gmail.com>
2017-04-05 18:37:05 +0300

    Update Exceptions

---
+++ ConsolePlugin/src/main/java/com/dronegcs/console_plugin/mission_editor/MissionUpdateException.java
@@ -0,0 +1,11 @@
+package com.dronegcs.console_plugin.mission_editor;
+
+/**
+ * Created by taljmars on 4/5/17.
+ */
+public class MissionUpdateException extends Exception {
+
+    public MissionUpdateException(String message) {
+        super(message);
+    }
+}
---
+++ ConsolePlugin/src/main/java/com/dronegcs/console_plugin/services/internal/convertors/MissionCompilationException.java
@@ -0,0 +1,12 @@
+package com.dronegcs.console_plugin.services.internal.convertors;
+
+/**
+ * Created by taljmars on 4/5/17.
+ */
+public class MissionCompilationException extends Exception {
+
+    public MissionCompilationException(String message) {
+        super(message);
+    }
+
+}
