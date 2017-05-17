e267635c024155c55582c34c0aa0309a1a431e83
taljmars <taljmars@gmail.com>
2017-05-17 19:30:48 +0300

    Setting dependencies

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-LOGS_DIR=../logs/
-CONF_DIR=../conf/
+LOGS_DIR=logs/
+CONF_DIR=conf/
 
 mkdir $LOGS_DIR
 
 sudo ln -s /dev/ttyACM0 /dev/ttyS85
-java -Djava.library.path=../lib -classpath /usr/local/jdk1.8.0_121/jre/lib/ext/jfxrt.jar:../jars/DroneGCS-1.0-SNAPSHOT.jar com.dronegcs.console.controllers.DroneLaunch $LOGS_DIR $CONF_DIR> $LOGS_DIR/backlog.elg
+java -Djava.library.path=../lib -cp /usr/local/jdk1.8.0_121/jre/lib/ext/jfxrt.jar:'*'  com.dronegcs.console.controllers.DroneLaunch $LOGS_DIR $CONF_DIR> $LOGS_DIR/backlog.elg
