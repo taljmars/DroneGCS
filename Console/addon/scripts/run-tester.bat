e979834bf0e60e5e6d6ff572878c511b50f4a853
taljmars <taljmars@gmail.com>
2018-03-29 23:05:00 +0300

    .

--- Console/addon/scripts/run-tester.bat
+++ Console/addon/scripts/run-tester.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-set LOGS_DIR=logs/
-set CONF_DIR=conf/
+@SET LOGS_DIR=logs/
+@SET CONF_DIR=conf/
 
 java -cp jars/* com.drone_tester.Tester %LOGS_DIR% %CONF_DIR% org.springframework.boot.loader.PropertiesLauncher -Dloader.path=lib/ -Djava.library.path=./native > %LOGS_DIR%/backlog.elg
